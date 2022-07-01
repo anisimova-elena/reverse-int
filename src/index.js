c774b31ee1083968cf9233cad0c2d06ae7048aac
Еlena Anisimova <elena.anis.12@yandex.ru>
2022-07-01 17:32:02 +0300

    working version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let i = 0;
+    if (n < 0) {
+        n *= -1;
+    }
+  while (n > 0) {
+      i = i * 10 + n % 10;
+      n = (n - n % 10) / 10;
+  }
+  return i;
 }
